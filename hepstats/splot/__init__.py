d261c843daa2595b85a9202f2143fb4d7d879b36
Matthieu Marinangeli <matthieu.marinangeli@epfl.ch>
2020-03-11 11:29:19 +0100

    add Eduardo's comments on Splot PR

--- hepstats/splot/__init__.py
+++ hepstats/splot/__init__.py
@@ -1,1 +1,9 @@
+"""
+Module implementing the **sPlot** algorithm [1]_.
+
+References
+----------
+.. [1] https://arxiv.org/abs/physics/0402083
+"""
+
 from .sweights import compute_sweights
---
+++ hepstats/utils/__init__.py
@@ -0,0 +1,1 @@
+from .fit import eval_pdf, array2dataset, pll, base_sampler, base_sample, get_value
