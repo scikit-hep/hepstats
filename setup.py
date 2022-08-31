2c5a44dc3ec47e6d7dd6592c1d6eac045420e23a
Jonas Eschle <mayou36@jonas.eschle.com>
2022-08-31 21:55:32 +0200

    chore: ensure setuptools is installed

--- setup.py
+++ setup.py
@@ -5,6 +5,10 @@
 # Distributed under the 3-clause BSD license, see accompanying file LICENSE
 # or https://github.com/scikit-hep/hepstats for details.
 
+# to check if setuptools_scm is installed
+# https://scikit-hep.org/developer/packaging#git-tags-official-pypa-method
+import setuptools_scm  # noqa: F401
+
 from setuptools import setup
 
 setup()
